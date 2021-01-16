b50f67280d323c181e85f8b25d7cc8018bceeacb
Rudrakshpnd <rudrabhakt.3@gmail.com>
2021-01-16 22:33:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.newsapp.Model.Articles;
+import com.example.newsapp.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+    final String API_KEY= "920400cf2e8143dd99450c07d864cfe9";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        swipeRefreshLayout = findViewById(R.id.swiperefresh);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String country = getCountry();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson(country,API_KEY);
+            }
+        });
+
+        retrieveJson(country,API_KEY);
+    }
+
+    public void retrieveJson(String country,String apiKey){
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country,apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if(response.isSuccessful() && response.body().getArticles() != null){
+                    swipeRefreshLayout.setRefreshing(false);
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this,articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
+}
---
+++ app/src/main/java/com/example/newsapp/Model/Headlines.java
@@ -0,0 +1,44 @@
+package com.example.newsapp.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Headlines {
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("totalResult")
+    @Expose
+    private String totalResult;
+
+    @SerializedName("articles")
+    @Expose
+    private List<Articles> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResult() {
+        return totalResult;
+    }
+
+    public void setTotalResult(String totalResult) {
+        this.totalResult = totalResult;
+    }
+
+    public List<Articles> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Articles> articles) {
+        this.articles = articles;
+    }
+}
